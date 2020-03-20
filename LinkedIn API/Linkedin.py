2d2dd607429d1f3e379ed75b8a00d499bee15028
deborahlee2018 <deborah.lee.2018@sis.smu.edu.sg>
2020-03-20 17:02:05 +0800

    linkedin.py

---
+++ LinkedIn API/Linkedin.py
@@ -0,0 +1,9 @@
+from linkedin import linkedin
+
+API_KEY = 'wFNJekVpDCJtRPFX812pQsJee-gt0zO4X5XmG6wcfSOSlLocxodAXNMbl0_hw3Vl'
+API_SECRET = 'daJDa6_8UcnGMw1yuq9TjoO_PMKukXMo8vEMo7Qv5J-G3SPgrAV0FqFCd0TNjQyG'
+RETURN_URL = 'http://localhost:5000'
+
+authentication = linkedin.LinkedInAuthentication(API_KEY, API_SECRET, RETURN_URL, linkedin.PERMISSIONS.enums.values())
+print authentication.authorization_url  # open this url on your browser
+application = linkedin.LinkedInApplication(authentication)
