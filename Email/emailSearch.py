1558b4fb4f2cec3c89c55fb0d1a750b57dfb21a2
ruipeng1 <54791609+ruipeng1@users.noreply.github.com>
2020-03-20 15:00:33 +0800

    Email Search
    Filter by industry and company base on UID

--- Email/emailSearch.py
+++ Email/emailSearch.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-
+from sqlalchemy import and_, or_, not_
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/nap'
@@ -39,17 +39,35 @@ class Email(db.Model):
         "company": self.company,"title": self.title,"industry": self.industry}
 
 
-@app.route("/email")
-def get_all():
-    return jsonify({"email": [email.json() for email in Email.query.all()]})
+# @app.route("/email/<string:uid>")
+# def get_all(uid):
+#     email = Email.query.filter_by(uid=uid).all()
+#     if email:
+#         return jsonify({"email": [emails.json() for emails in email]})
+#     return jsonify({"message": "======"})
 
 
-@app.route("/email/<string:company>")
-def find_by_isbn13(company, industry):
-    email = Email.query.filter_by(company=company, industry=industry).all()
+@app.route("/email/<string:uid>&<string:company>&<string:industry>")
+def find_by_company_indust(uid, company, industry):
+    email = Email.query.filter_by(uid=uid, company=company, industry=industry).all()
     if email:
         return ({"email": [emails.json() for emails in email]})
     return jsonify({"message": "______ not found."}), 404
+    
+@app.route("/email/<string:uid>&<string:either>")
+def find_by_companyOrindust(uid,either):
+    email = Email.query.filter(Email.uid==uid,(or_(Email.company.like('%'+either+'%'), Email.industry.like('%'+either+'%')))).all()
+    if email:
+        return ({"email": [emails.json() for emails in email]})
+    return jsonify({"message": "______ not found."}), 404
+
+# @app.route("/email/<string:uid>&<string:either>")
+# def find_by_either(either):
+#     email = Email.query.filter(
+#         and_(uid=uid,((company=either | title=either, industry=either))))
+#     if len(email.all()) != 0:
+#         return ({"email": [emails.json() for emails in email]})
+#     return jsonify({"message": "______ not found."}), 404
 
 
 # @app.route("/book/<string:isbn13>", methods=['POST'])
