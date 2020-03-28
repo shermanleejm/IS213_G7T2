7b6b27c44c1e0562b51fcf0d67e112912308dcfa
shishiong <54533935+shishiong@users.noreply.github.com>
2020-03-28 14:20:52 +0800

    update docker for user

--- login/user.py
+++ login/user.py
@@ -14,10 +14,10 @@ CORS(app)
 
 
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') 
 # add the above code if docker is used 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/nap'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') 
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/nap'
 db = SQLAlchemy(app)
 
 
