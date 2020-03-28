e5d7f588586d7c7746df30cc5b4299cafed096ed
shishiong <54533935+shishiong@users.noreply.github.com>
2020-03-28 11:04:39 +0800

    docker
    docker

--- Email/emailSearch.py
+++ Email/emailSearch.py
@@ -93,4 +93,4 @@ def sendEmail(uid, emailcheck, emailsubject, emailmessage,senderEmail,emailPassw
             message_array=[]
     return jsonify({"message": "Email(s) sent."})
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host = '0.0.0.0', port=5000, debug=True)
