7aad7f342178b4e476de3f82645742f5e3aacbfc
shermanleejm <leesherman@live.com.sg>
2020-03-31 17:36:32 +0800

    Update User.sql

--- login/User.sql
+++ login/User.sql
@@ -8,11 +8,12 @@ CREATE TABLE users (
   name char(100) NOT NULL,
   email varchar(100) NOT NULL,
   password text(1000) NOT NULL,
+  emailpassword text(1000) NOT NULL,
   PRIMARY KEY (uid)
 );
 
 INSERT INTO users VALUES
-("deebo", 'Deborah Lee', 'deborah.lee.2018@sis.smu.edu.sg', 'apple123'),
-("rach", 'Rachel Lui', 'rachel.lui.2018@sis.smu.edu.sg', 'orange123'),
-("sherman", "Lee Sherman", "sherman.lee.2018@sis.smu.edu.sg", "shermanrox")
+("deebo", 'Deborah Lee', 'deborah.lee.2018@sis.smu.edu.sg', 'apple123', "password1!"),
+("rach", 'Rachel Lui', 'rachel.lui.2018@sis.smu.edu.sg', 'orange123', "password1!"),
+("sherman", "Lee Sherman", "sherman.lee.2018@sis.smu.edu.sg", "shermanrox", "password1!")
 ;
