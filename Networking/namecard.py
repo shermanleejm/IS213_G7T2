f997ce7cc361846bb360be9e1beeae44ab1df9dc
shermanleejm <leesherman@live.com.sg>
2020-04-02 13:54:26 +0800

    updated docker files

--- Networking/namecard.py
+++ Networking/namecard.py
@@ -6,7 +6,7 @@ from os import environ
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://nap@localhost:3306/namecard'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://nap@localhost:3306/namecard'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
