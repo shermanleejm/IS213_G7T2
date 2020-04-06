7d961276077540006aed39ea48887f920ed1bd2a
shermanleejm <leesherman@live.com.sg>
2020-04-06 17:49:22 +0800

    changed for local use

--- Networking/namecard.py
+++ Networking/namecard.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://nap@localhost:3306/namecard'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#engine = create_engine(environ.get('dbURL'))
-# engine = create_engine('mysql+mysqlconnector://nap@localhost:3306/namecard')
+# engine = create_engine(environ.get('dbURL'))
+engine = create_engine('mysql+mysqlconnector://nap@localhost:3306/namecard')
 
 db = SQLAlchemy(app)
 CORS(app)
